019796daeb028fcc730950fb8bab3250b6a2e88b
Stanislav Ivanov <135168930+StanislavIvanov7@users.noreply.github.com>
2023-12-24 14:29:34 +0200

    Add files via upload

---
+++ 03.Entity Relations/02.Football Betting/Data/Models/Town.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P02_FootballBetting.Data.Models;
+
+public class Town
+{
+    public Town()
+    {
+        Teams = new HashSet<Team>();
+    }
+
+    [Key]
+    public int TownId { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+
+    public int CountryId { get; set; }
+
+    [ForeignKey(nameof(CountryId))]
+    public virtual Country Country { get; set; }
+    public ICollection <Player> Players { get; set; }
+
+    public virtual ICollection<Team> Teams { get; set; }
+}
