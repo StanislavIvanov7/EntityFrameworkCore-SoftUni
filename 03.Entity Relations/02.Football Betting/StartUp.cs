fcf1b023fb5f0b7d24912409f0d6ebaa89bb6361
Stanislav Ivanov <135168930+StanislavIvanov7@users.noreply.github.com>
2023-12-24 14:31:47 +0200

    Add files via upload

---
+++ 03.Entity Relations/02.Football Betting/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P02_FootballBetting.Data;
+
+namespace P02_FootballBetting
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            FootballBettingContext footballBettingContext = new FootballBettingContext();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
