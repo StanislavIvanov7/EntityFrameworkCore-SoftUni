cb0e670a47bda1353e2dc4b3be1fc917735d43e5
Stanislav Ivanov <135168930+StanislavIvanov7@users.noreply.github.com>
2023-12-24 14:25:19 +0200

    Add files via upload

---
+++ 03.Entity Relations/01.Student System/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using P01_StudentSystem.Data;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
