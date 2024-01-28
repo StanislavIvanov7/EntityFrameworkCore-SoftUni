5e91454814ae8129a48109cf711c7e4aba32d9ee
Stanislav Ivanov <135168930+StanislavIvanov7@users.noreply.github.com>
2024-01-28 10:03:07 +0200

    Add files via upload

---
+++ 05.ExerciseAdvancedQuerying/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString
+            => "Server=DESKTOP-R3DB24I\\SQLEXPRESS;Database=BookShop;Integrated Security=True;";
+    }
+}
