2c8dedfcb65fa6660bf0bf367c0afbf34b45af7c
Stanislav Ivanov <135168930+StanislavIvanov7@users.noreply.github.com>
2024-01-29 15:57:23 +0200

    Add files via upload

---
+++ 06.ExerciseAutoMappingObjects/FastFood.Core/ViewModels/Orders/CreateOrderInputModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FastFood.Core.ViewModels.Orders
+{
+    public class CreateOrderInputModel
+    {
+        public string Customer { get; set; }
+
+        public string Item { get; set; }
+
+        public string Employee { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
