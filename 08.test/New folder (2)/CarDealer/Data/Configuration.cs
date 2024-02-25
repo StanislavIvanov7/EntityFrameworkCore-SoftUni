82fac10e0aa4a67aa26aa352d8d018d92abeec35
Stanislav Ivanov <135168930+StanislavIvanov7@users.noreply.github.com>
2024-02-25 10:18:58 +0200

    Delete 08.test/New folder (2) directory

--- 08.test/New folder (2)/CarDealer/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CarDealer.Data
-{
-    public static class Configuration
-    {
-        public const string ConnectionString = @"Server=DESKTOP-R3DB24I\SQLEXPRESS;Database=CarDealer;Integrated Security=True;Encrypt=False";
-    }
-}
