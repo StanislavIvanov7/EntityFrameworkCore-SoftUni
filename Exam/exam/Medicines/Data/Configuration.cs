ff9018a1b669d828896c28a20abaf1d618d9ccea
Stanislav Ivanov <135168930+StanislavIvanov7@users.noreply.github.com>
2024-02-25 10:19:20 +0200

    Delete Exam/exam directory

--- Exam/exam/Medicines/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Medicines.Data
-{
-    public class Configuration
-    {
-        public static string ConnectionString = @"Server=DESKTOP-R3DB24I\SQLEXPRESS;Database=Medicines;Integrated Security=True;Encrypt=False";
-    }
-}
